363b2495203306af369e4375fe8789ca14bd22e1
jaumecolomhernandez <jaumecolomhernandez@gmail.com>
2019-11-16 13:40:45 +0100

    Added mwc_scraper

---
+++ mwc_scraper/README.md
@@ -0,0 +1,2 @@
+# MwcScraper
+A Scraper to get the information of all the exhibitors in the MWC
---
+++ mwc_scraper/scraper_mwc.py
@@ -0,0 +1,3 @@
+"""Script per a descarregar totes les empresses assistents al mobile -- Jaume Colom"""
+
+import requests
