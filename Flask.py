9a86b7fe5b93271fd03e61b82827666c037fbfd5
zombie2113 <abidawan42@gmail.com>
2019-11-17 02:32:27 +0000

    Update 2:32

--- Flask.py
+++ Flask.py
@@ -15,8 +15,13 @@ def hello_worlds():
    urls = [("Hungry boi, invest now frens", "https://i.redd.it/uvenlxagz1z31.jpg"),
             ("Rick and Morty formats are always great choice. Stop asking questions and invest in this format!","https://i.redd.it/7rora8l032z31.jpg"),
             ("Do it! Invest in the most important super hero of all time. So you always can bury good corpses..","https://i.redd.it/vv5arx2bw0z31.jpg"),
-            ("Fresh template, invest right now!","https://i.redd.it/2pd4lwd511z31.jpg")]
-   vals = np.random.choice(data.url.shape[0], 4)
+            ("Fresh template, invest right now!","https://i.redd.it/2pd4lwd511z31.jpg"), ("Fresh template, invest right now!", "https://i.redd.it/2pd4lwd511z31.jpg")
+            , ("Invest in brand new meme template","https://i.redd.it/l8h48opu42z31.jpg"),("Fresh Trump template, invest now!","https://i.redd.it/7xaj1pigg2z31.jpg"),
+            ("Veyr cash money","https://i.redd.it/binbz7w7u0z31.jpg"),("Invest in the broken dam","https://i.redd.it/u50ecmr04yy31.jpg"),
+            ("You gotta love Keanu Reeves formats!", "Invest in this new meme format!,https://i.redd.it/"),("Invest in my dad and brother","https://i.redd.it/3fa0vk33jwy31.jpg"),
+            ("Invest today!","https://i.redd.it/wi5b15u1m2z31.jpg"),("Dope or Nope meme format! Invest now!","https://i.redd.it/d29veri8l0z31.jpg"),
+            ("Invest before becoming a victim to the next emergency,https://i.redd.it/b9dz1hulsxy31.jpg")]
+   vals = np.random.choice(data.url.shape[0], 15)
    urls = list(data.url[vals])
    return jsonify(urls)
 
