d8567e5ac48575ad1f9e75586a5022d3166ab1fa
jaumecolomhernandez <jaumecolomhernandez@gmail.com>
2019-11-17 03:24:10 +0100

    Added counter anb button

--- static/logic.js
+++ static/logic.js
@@ -1,9 +1,35 @@
-async function myFunction(){
+
+//var response2;
+
+async function myFunction() {
     let headingElement = document.querySelector("#photo_wrapper");
-    const response2 = await fetch('/api').then(response => response.json());
-    //headingElement.textContent = response2;
-    document.getElementById("photo1").src = response2[0];
-    document.getElementById("photo2").src = response2[1];
-    document.getElementById("photo3").src = response2[2];
-    document.getElementById("photo4").src = response2[3];
+    response2 = await fetch('/api').then(response => response.json());
+    window.info = response2
+    console.log(response2)
+    for (i = 0; i < 4; i++) {
+        document.getElementById("photo"+String(i+1)).src = response2[i][1];
+        document.getElementById("phototext"+String(i+1)).textContent = response2[i][2];
+    }
+}
+
+async function vote_photo(number) {
+    //console.log(window.info[number - 1][0]);
+    console.log(window.info);
+    const idstr = window.info[number - 1][0];
+    let rawResponse = await fetch('/api/vote', {
+        method: "POST",
+        body: "id="+String(idstr),
+        headers:
+        {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
+
+    window.info[number-1][2] = window.info[number-1][2]+1;
+
+    //body: JSON.stringify({ id: idstr }), // data can be `string` or {object}!
+    document.getElementById("phototext"+String(number)).textContent = window.info[number-1][2];
+    const content = await rawResponse.json();
+    //alert(content.message);
+
 }
